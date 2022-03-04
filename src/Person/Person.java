78699d004e69c6c4c671d26cb304ee8c6d1b642c
Florian Peer <fpeer21@gmail.com>
2022-03-04 11:07:14 +0100

    Grundaufbau Personen erstellt

---
+++ src/Person/Person.java
@@ -0,0 +1,82 @@
+package Person;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Person{
+
+    private int _id;
+    private String _firstname;
+    private String _lastname;
+    private LocalDate _birthdate;
+    private char _geschlecht;
+
+
+
+    public int getId() {
+        return this._id;
+    }
+
+    public void setId(int id) {
+        if (id >= 0) {
+            this._id = id;
+        }
+    }
+
+    public String getFirstname() {
+
+        return this._firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this._firstname = firstname;
+    }
+
+    public String getLastname() {
+        return this._lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this._lastname = lastname;
+    }
+
+    public LocalDate getBirthdate() {
+        return this._birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this._birthdate = birthdate;
+    }
+
+    public char getGeschlecht() {
+        return this._geschlecht;
+    }
+
+    public void setGeschlecht(char geschlecht) {
+        this._geschlecht = geschlecht;
+    }
+
+
+    public Person() {
+        this(0, " ", " ", null, ' ');
+    }
+
+    public Person(int id, String firstname, String lastname, LocalDate birthdate, char geschlecht) {
+        this.setId(id);
+
+        this.setFirstname(firstname);
+        //this._firstname = firstname;
+
+        this.setLastname(lastname);
+        this.setBirthdate(birthdate);
+        this.setGeschlecht(geschlecht);
+    }
+
+    @Override
+    public String toString() {
+        return this._id + " " + this._firstname + " " + this._lastname + "\n" + this._birthdate + " " + this._geschlecht;
+    }
+
+
+}
