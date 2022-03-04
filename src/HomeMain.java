31d4a5a223112eb15e32c035b036903fff98e757
MaxiStrubi <maxi.stebler@gmail.com>
2022-03-04 11:26:53 +0100

    HomeMain erstellt

--- src/HomeMain.java
+++ src/HomeMain.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class HomeMain {
 
+    static Scanner reader = new Scanner(System.in);
+    static char choice;
+    static String username;
+    static String password;
 
-    
+    public static void main(String[] args) {
+        System.out.println("Willkommen");
+        System.out.println("Dies ist nur ein Versuch");
+
+        System.out.println("Wollen Sie sich anmelden [a] /registrieren [r] oder als gast [g] fortfahren?");
+        choice = reader.next().toLowerCase().charAt(0);
+        switch (choice){
+            case 'a':
+                System.out.println("Anmelden");
+                anmelden();
+                break;
+            case 'r':
+                System.out.println("Registrieren");
+                break;
+            case 'g':
+                System.out.println("Gast");
+                break;
+        }
+
+
+
+    }
+    public static boolean anmelden(){
+        System.out.println("Anmeldungsbereich");
+        System.out.println("[Benutzername:]");
+
+        System.out.println("[Passwort]");
+        return true;
+    }
 }
