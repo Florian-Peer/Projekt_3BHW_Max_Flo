37790e11cca93e1c6aff8ca8a5af46275068656c
Florian Peer <fpeer21@gmail.com>
2022-03-11 11:29:09 +0100

    "test"

--- src/Person/Customer/payingCustomer/payingCustomer.java
+++ src/Person/Customer/payingCustomer/payingCustomer.java
@@ -2,9 +2,13 @@ package Person.Customer.payingCustomer;
 
 import Person.Customer.Customer;
 
+import java.time.LocalDate;
+
 public class payingCustomer extends Customer {
+    String was;
 
 
-
-
+    public payingCustomer(int id, String firstname, String lastname, LocalDate birthdate, char gender) {
+        super(id, firstname, lastname, birthdate, gender);
+    }
 }
