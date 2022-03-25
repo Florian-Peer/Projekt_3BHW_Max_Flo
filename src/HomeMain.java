5011cdfb269628d82d161dd56f90fea788aa501c
Florian Peer <fpeer21@gmail.com>
2022-03-25 11:30:35 +0100

    reader -> leser

--- src/HomeMain.java
+++ src/HomeMain.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class HomeMain {
 
-    static Scanner reader = new Scanner(System.in);
+    static Scanner leser = new Scanner(System.in);
     static char choice;
     static char choice2;
     static char mmenu;
@@ -21,16 +21,10 @@ public class HomeMain {
         System.out.println("Dies ist nur ein Versuch");
 
         System.out.println("Wollen Sie sich anmelden [a] /registrieren [r] oder als gast [g] fortfahren?");
-        choice = reader.next().toLowerCase().charAt(0);
+        choice = leser.next().toLowerCase().charAt(0);
         switch (choice){
             case 'a':
                 System.out.println("Anmelden");
-                if(anmelden()){
-                    System.out.println("Anmelden erfolgreich!");
-                }
-                else {
-                    System.out.println("Anmelden fehlgeschlagen!");
-                }
                 //Hier wird probiert
                 if(anmelden()){
                     System.out.println("Supper das hat geklappt");
@@ -38,7 +32,7 @@ public class HomeMain {
                     System.out.println("M----Mitarbeiterbereich");
                     //Mitarbeiter ID angeben
                     System.out.println("B----Besucherbereich");
-                    choice2 = reader.next().toLowerCase().charAt(0);
+                    choice2 = leser.next().toLowerCase().charAt(0);
                     switch(choice2){
                         case 'm':
                             if(mitarbeiterId()){
@@ -46,7 +40,7 @@ public class HomeMain {
                                 System.out.println("_-_-_-_-_-_-Mitarbeiterbereich-_-_-_-_-_-_-_\n\n");
                                 System.out.println("Sie können nun auf folgende Funktionen zugreifen");
                                 System.out.println("[S] Schneekanonen steuern\n\n");
-                                mmenu = reader.next().toLowerCase().charAt(0);
+                                mmenu = leser.next().toLowerCase().charAt(0);
                                 switch (mmenu){
                                     case 's':
                                         System.out.println("Schneekanonen werden geladen....\n");
@@ -85,9 +79,9 @@ public class HomeMain {
     public static boolean anmelden(){
         System.out.println("Anmeldungsbereich");
         System.out.println("[Benutzername:]");
-        username = reader.next();
+        username = leser.next();
         System.out.println("[Passwort]");
-        password=reader.next();
+        password= leser.next();
 
         HashMap<String,String> tempMap = readCsvIntoHashmap(userData);
 
@@ -151,9 +145,9 @@ public class HomeMain {
         System.out.println("neuen Benutzer erstellen\n");
 
         System.out.println("Was ist der Benutzername?");
-        username = reader.next();
+        username = leser.next();
         System.out.println("Was ist das Passwort?");
-        password = reader.next();
+        password = leser.next();
 
         if(userdata.containsValue(username)){
             System.out.println("Benutzer existiert schon!");
