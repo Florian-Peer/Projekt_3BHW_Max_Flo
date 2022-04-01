c044280a5156abc181e6a5cea26da8e2c06c16fa
Florian Peer <fpeer21@gmail.com>
2022-04-01 10:33:21 +0200

    Registrieren funktioniert wirklich hab anmelden probiert aber klappt nocht nicht - Hashmap(KEY,VALUE), nicht Hashmap(VALUE,KEY)!!!

--- src/HomeMain.java
+++ src/HomeMain.java
@@ -19,26 +19,26 @@ public class HomeMain {
 
         System.out.println("Wollen Sie sich anmelden [a] /registrieren [r] oder als gast [g] fortfahren?");
         choice = reader.next().toLowerCase().charAt(0);
-        switch (choice){
+        switch (choice) {
             case 'a':
                 System.out.println("Anmelden");
                 //Hier wird probiert
-                if(anmelden()){
+                if (anmelden()) {
                     System.out.println("Supper das hat geklappt");
                     System.out.println("Wie wollen Sie nun fortfahren?");
                     System.out.println("M----Mitarbeiterbereich");
                     //Mitarbeiter ID angeben
                     System.out.println("B----Besucherbereich");
                     choice2 = reader.next().toLowerCase().charAt(0);
-                    switch(choice2){
+                    switch (choice2) {
                         case 'm':
-                            if(mitarbeiterId()){
+                            if (mitarbeiterId()) {
                                 System.out.println("\n\n\n\n");
                                 System.out.println("_-_-_-_-_-_-Mitarbeiterbereich-_-_-_-_-_-_-_\n\n");
                                 System.out.println("Sie können nun auf folgende Funktionen zugreifen");
                                 System.out.println("[S] Schneekanonen steuern\n\n");
                                 mmenu = reader.next().toLowerCase().charAt(0);
-                                switch (mmenu){
+                                switch (mmenu) {
                                     case 's':
                                         System.out.println("Schneekanonen werden geladen....\n");
                                         System.out.println();
@@ -51,8 +51,7 @@ public class HomeMain {
 
                     }
 
-                }
-                else{
+                } else {
                     System.out.println("Fehler bei der Anmeldung");
                 }
 
@@ -73,88 +72,96 @@ public class HomeMain {
 
     // TODO: 25.03.2022 anmelden lösen
 
-    public static boolean anmelden(){
+    public static boolean anmelden() {
         System.out.println("Anmeldungsbereich");
         System.out.println("[Benutzername:]");
         username = reader.next();
         System.out.println("[Passwort]");
-        password= reader.next();
+        password = reader.next();
 
-        HashMap<String,String> tempMap = readCsvIntoHashmap(userData);
+        HashMap<String, String> tempMap = readCsvIntoHashmap(userData);
 
         for (String i : tempMap.keySet()) {
             System.out.println(i);
         }
 
-        if(tempMap.get(username) == password){
+        if (tempMap.get(username) == password) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    public static boolean registrieren(Path p){
+    public static boolean registrieren(Path p) {
         System.out.println("Registrierbereich");
-        if(writeToFile(p,inputHashmap(readCsvIntoHashmap(p)))){
+        if (writeToFile(p, inputHashmap(readCsvIntoHashmap(p)))) {
             System.out.println("registriert");
             return true;
         }
         return false;
     }
-    public static boolean mitarbeiterId(){
+
+    public static boolean mitarbeiterId() {
         System.out.println("Sie sind ein Mitarbeiter");
         return true;
     }
 
-    public static HashMap<String, String> readCsvIntoHashmap(Path p){
+    public static HashMap<String, String> readCsvIntoHashmap(Path p) {
         List<String> tempStr = new ArrayList<String>();
-        HashMap<String,String> tempmap = new HashMap<String,String>();
+        HashMap<String, String> tempmap = new HashMap<String, String>();
 
-        try{
+        try {
             tempStr = Files.readAllLines(p);
         } catch (IOException e) {
             System.out.println("Fehler beim lesen der Datei");
             return null;
         }
 
-        for(int i=0; i< tempStr.size();i++){
+        for (int i = 0; i < tempStr.size(); i++) {
             String[] zuSpalten = tempStr.get(i).split(";");
-            tempmap.put(zuSpalten[0],zuSpalten[1]);
+            tempmap.put(zuSpalten[0], zuSpalten[1]);
         }
         return tempmap;
 
     }
 
-    public static boolean writeToFile(Path p, String inputData){
+    public static boolean writeToFile(Path p, String inputData) {
         try {
             Files.writeString(p, inputData, StandardOpenOption.APPEND);
             return true;
-        }catch(IOException e) {
+        } catch (IOException e) {
             System.out.println("Fehler beim schreiben!\n");
             e.printStackTrace();
             return false;
         }
     }
 
-    public static String inputHashmap(HashMap<String,String> userdata){
+    public static String inputHashmap(HashMap<String, String> userdata) {
+        char choice;
+        boolean retry = true;
 
-        System.out.println("neuen Benutzer erstellen\n");
+        do {
+            System.out.println("neuen Benutzer erstellen\n");
 
-        System.out.println("Was ist der Benutzername?");
-        username = reader.next();
-        System.out.println("Was ist das Passwort?");
-        password = reader.next();
+            System.out.println("Was ist der Benutzername?");
+            username = reader.next();
+            System.out.println("Was ist das Passwort?");
+            password = reader.next();
 
-        if(userdata.containsValue(username)){
-            System.out.println("Benutzer existiert schon!");
-        }
-        else {
-            System.out.println("Daten werden weitergegeben ...");
-            return username + ";" + password +"\n";
+            if (userdata.containsKey(username)) {
+                System.out.println("Benutzer existiert schon!");
+                System.out.println("erneut versuchen? [j/n]");
+                choice = reader.next().toLowerCase().charAt(0);
+                if (choice == 'n') {
+                    retry = false;
+                }
 
-        }
-
+            } else {
+                System.out.println("Daten werden weitergegeben ...");
+                return username + ";" + password + "\n";
+            }
+        } while (retry);
         return null;
+
     }
 }
